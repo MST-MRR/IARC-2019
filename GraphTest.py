2cb25787c858bcd8466130c2df9e459078dde648
Thomas McKanna <tjmzq8@mst.edu>
2018-09-08 10:33:41 -0500

    Added a file which outlines command line arguments and code to test them.

--- GraphTest.py
+++ GraphTest.py
@@ -8,7 +8,13 @@ def main(argv):
     pan_window = -1;
 
     try:
-        opts, args = getopt.getopt(argv, "p:", ["pan="])
+        opts, args = getopt.getopt(argv, 
+                "p:s:", 
+                ["pan=", 
+                "save=", 
+                "pitch=", 
+                "yaw=" , 
+                "roll="])
     except getopt.GetoptError:
         print('GraphTest.py -p <pan_window>')
         sys.exit()
@@ -17,16 +23,24 @@ def main(argv):
             will_pan = True;
             # catch error if cast fails?
             pan_window = int(arg)
+        elif opt in ("-s", "--save"):
+            print(arg)
+        elif opt == "--pitch":
+            print(arg)
+        elif opt == "--yaw":
+            print(arg)
+        elif opt == "--roll":
+            print(arg)
 
     print("Pan is activated: ", will_pan)
     print("Pan window: ", pan_window)
-
+""" #Uncomment this to test the graph
     if pan_window > 0:
         test_grapher = Grapher(pan_window)
         test_grapher.run()
     else:
         test_grapher = Grapher()
         test_grapher.run()
-
+"""
 if __name__ == "__main__":
     main(sys.argv[1:])
