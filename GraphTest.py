9cf56ffbfea7993982c6bc177e9ed818833536f7
Thomas McKanna <tjmzq8@mst.edu>
2018-09-08 12:48:10 -0500

    Cleaned up command line argument code and added a usage string.

--- GraphTest.py
+++ GraphTest.py
@@ -3,20 +3,25 @@ import getopt
 
 from ProGraphs import Grapher
 
+usage_string = \
+'''Usage: <program_name> [-p <pan_window>] [-s <output_file] [--pitch <target>] [--yaw <target>] [--roll <target>]
+
+    -p:       Sets the width of the panning window (in seconds). 
+    -s:       Saves graphing output to the specified file.
+    --pitch   Creates a subplot for pitch data with target
+    --yaw     Creates a subplot for yaw data with target
+    --roll    Creates a subplot for roll data with target'''
+
 def main(argv):
     will_pan = False;
     pan_window = -1;
 
     try:
         opts, args = getopt.getopt(argv, 
-                "p:s:", 
-                ["pan=", 
-                "save=", 
-                "pitch=", 
-                "yaw=" , 
-                "roll="])
+                "p:s:", ["pan=", "save=", 
+                    "pitch=", "yaw=" , "roll="])
     except getopt.GetoptError:
-        print('GraphTest.py -p <pan_window>')
+        print(usage_string)
         sys.exit()
     for opt, arg in opts:
         if opt in ("-p", "--pan"):
@@ -24,23 +29,26 @@ def main(argv):
             # catch error if cast fails?
             pan_window = int(arg)
         elif opt in ("-s", "--save"):
-            print(arg)
+            # TODO: Implement save feature
+            pass
         elif opt == "--pitch":
-            print(arg)
+            # TODO: Implement subplot (same for next two options)
+            pass
         elif opt == "--yaw":
-            print(arg)
+            # TODO
+            pass
         elif opt == "--roll":
-            print(arg)
+            # TODO
+            pass
 
     print("Pan is activated: ", will_pan)
     print("Pan window: ", pan_window)
-""" #Uncomment this to test the graph
     if pan_window > 0:
         test_grapher = Grapher(pan_window)
         test_grapher.run()
     else:
         test_grapher = Grapher()
         test_grapher.run()
-"""
+
 if __name__ == "__main__":
     main(sys.argv[1:])
