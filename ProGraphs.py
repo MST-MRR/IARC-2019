582e9352f237645598d6af9cb1ea4604115f7ab2
Thomas McKanna <tjmzq8@mst.edu>
2018-09-08 23:26:33 -0500

    Made it so that rather than completely remake axes (labels and all) every iteration, it just removes the last line drawn.

--- ProGraphs.py
+++ ProGraphs.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import time
 
+from weakref import ref
+
 from ProValueGenerator import getNextValue
 
 class Grapher(object):
@@ -23,16 +25,19 @@ class Grapher(object):
         if 'pitch' in graph:
             self.graph.append('pitch')
             self.ax_p = self.figure.add_subplot(numrows, 1, current)
+            self.ax_p.set_title('Pitch')
             self.df_p = pd.DataFrame({"x":[], "y":[]})
             current += 1
         if 'yaw' in graph:
             self.graph.append('yaw')
             self.ax_y = self.figure.add_subplot(numrows, 1, current)
+            self.ax_y.set_title('Yaw')
             self.df_y = pd.DataFrame({"x":[], "y":[]})
             current += 1
         if 'roll' in graph:
             self.graph.append('roll')
             self.ax_r = self.figure.add_subplot(numrows, 1, current)
+            self.ax_r.set_title('Roll')
             self.df_r = pd.DataFrame({"x":[], "y":[]})
             current += 1
  
@@ -42,15 +47,21 @@ class Grapher(object):
             self.checkPoint = self.startTime
 
     def dispatch_update(self, ax, df, new_x, tag):
+        # 0th line is the target line
+        # 1st line is the old line
+        if len(ax.lines) > 1:
+            ax.lines[1].remove()
+
         df = df.append({"x":new_x, "y":getNextValue(new_x)}
                 , ignore_index=True)
-        ax.clear()
-        df.plot(x="x", y="y", ax=ax)
+        ax.set_xlim(auto=True)
+        df.plot(x="x", y="y", ax=ax, 
+                legend=None, color='blue')
 
         if self.pan > 0:
             left, right = ax.get_xlim()
             if right > self.pan:
-                ax.set_xlim(right - self.pan, right)
+                ax.set_xlim(left=right - self.pan)
   
         return df
         
@@ -65,10 +76,11 @@ class Grapher(object):
             if element == 'yaw':
                 self.df_y = self.dispatch_update(self.ax_y, 
                         self.df_y, diff, 'y')
+                self.ax_y.set_title('Yaw')
             if element == 'roll':
                 self.df_r = self.dispatch_update(self.ax_r, 
                         self.df_r, diff, 'r')
-        #plt.draw()
+                self.ax_r.set_title('Roll')
 
         if self.save:
             # save every 5 seconds
