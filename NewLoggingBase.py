599862ed561291e7aaed148bc65dbddc7114149e
Jonathan Ogden <jozgd@mst.edu>
2018-10-02 19:34:46 -0500

    stuff

--- NewLoggingBase.py
+++ NewLoggingBase.py
@@ -13,7 +13,7 @@ class Logger:
 
     def __init__(self):
         i = 0
-        
+        self.lastTime = 0
         date = time.strftime('%x') #Gets todays date
         newDate = date.replace('/','_') #changes the / for _ to not mess us the directory
         dailyFlight = 1
@@ -23,43 +23,46 @@ class Logger:
             if not os.path.exists(self.directory): #checks to see if the path already exists
                 open(self.directory,'a').close #if it doesn't exist it makes it
                 i = 5
-            else:
+            else:# keep adding the daily flight num till a new one is found
                 dailyFlight = dailyFlight + 1
-        self.start = time.time()
-        with open(self.directory,"w") as f:
+        self.start = time.time()#gets initial time
+        with open(self.directory,"w") as f:#opens the file to write the headers
             self.dataValues = ['secFromStart','airSpeed','altitude','pitch','roll','yaw','xVelocity','yVelocity','zVelocity','voltage'] #defining the headers
             writer = csv.DictWriter(f, fieldnames=self.dataValues) #definng the headers
             writer.writeheader()
-        self.g = open(self.directory,"a")
+        self.g = open(self.directory,"a")#Open the file in appending to add the new data
     
-    def exit(self):
+    def exit(self):#defines the funtion to close the file
         self.g.close()
 
-    def Update(self,inputData):    
+    def Update(self,inputData):#updates the file with the new data
              
                 currentTime = time.time()
-            
-                writer = csv.DictWriter(self.g, fieldnames=self.dataValues)
-                writer.writerow({
-                'secFromStart' : (currentTime - self.start),
-                'airSpeed' : inputData['airspeed'],
-                'altitude' : inputData['altitude'],
-                'pitch' : inputData['pitch'],
-                'roll' : inputData['roll'],
-                'yaw' : inputData['yaw'],
-                'xVelocity' : inputData['velocity_x'],
-                'yVelocity' : inputData['velocity_y'],
-                'zVelocity' : inputData['velocity_z'],
-                'voltage' : inputData['voltage']
-                })
-                #This is white the variable in the section titled at the left of the ':'
+
+                if (self.lastTime != currentTime):
+                    writer = csv.DictWriter(self.g, fieldnames=self.dataValues)
+                    writer.writerow({
+                    'secFromStart' : (currentTime - self.start),#the time stamp for the data
+                    'airSpeed' : inputData['airspeed'], #the headers to store
+                    'altitude' : inputData['altitude'],
+                    'pitch' : inputData['pitch'],
+                    'roll' : inputData['roll'],
+                    'yaw' : inputData['yaw'],
+                    'xVelocity' : inputData['velocity_x'],
+                    'yVelocity' : inputData['velocity_y'],
+                    'zVelocity' : inputData['velocity_z'],
+                    'voltage' : inputData['voltage']
+                    })
+                    #This is white the variable in the section titled at the left of the ':'
+
+                self.lastTime = currentTime
 
 
 my_logger = Logger()
 
 
 myData = {}
-while (x<theTempCounter):
+while (x<theTempCounter):#main loop
     #make data dictionary here
     myData = {
         'airspeed' : 1*x,
@@ -73,6 +76,5 @@ while (x<theTempCounter):
         'voltage' : 9*x,
     }
     my_logger.Update(myData)
-    time.sleep(0.0000000001)
     x = x + 1
 my_logger.exit()
--- csvFileGrapher.py
+++ csvFileGrapher.py
@@ -12,25 +12,25 @@ import matplotlib.dates as mdates
 root = Tk()
 root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file to Graph",filetypes = (("csv files","*.csv"),("all files","*.*")))
 fileToUse = root.filename
-
+#pick the file for the settings
 root.settings = filedialog.askopenfilename(initialdir = "/",title = "Select the Graphing Config file",filetypes = (("csv files","*.csv"),("all files","*.*")))
 configFile = root.settings
 
 df = pd.read_csv(fileToUse)
 df.head()
 
-with open(configFile, 'r') as s:
+with open(configFile, 'r') as s:#makes the config file into a list of lists
     reader = csv.reader(s)
     configMainList = list(reader)
 
-configList = configMainList[0]
-intervalList = configMainList[1]
+configList = configMainList[0] #pulls the first list of which headers to graph together
+intervalList = configMainList[1] #pulls the second list of what time interval to graph
 i = 0
 j = len(configList)
-minLimit = float(intervalList[0])
+minLimit = float(intervalList[0])#gets the min and max limts for graphing
 maxLimit = float(intervalList[1])
 
-while (i < j):
+while (i < j):#plots each data point based on the other settings
     dataToPlot = configList[i]
     maxTime = df['secFromStart'] < maxLimit    
     minTime = df['secFromStart'] > minLimit
@@ -41,5 +41,5 @@ while (i < j):
     plt.plot(x,y)
     i = i + 1
 
-
-plt.show()
+plt.legend()
+plt.show()#Show the plot
