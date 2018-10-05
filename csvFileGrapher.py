0f5f4deb2b719b36e304a8803576092b3467045b
csdhv9 <csdhv9@mst.edu>
2018-10-04 19:26:17 -0500

    Updated grapher.

--- csvFileGrapher.py
+++ csvFileGrapher.py
@@ -14,50 +14,46 @@ def make_whole_graph():
 
     #
     # Get startup data
-
     root = Tk()  # Create tkinter window
 
     # Select data to graph through explorer prompt
-    fileToUse = filedialog.askopenfilename(
-        initialdir="", title="Select file to Graph", filetypes=(("csv files", "*.csv"), ("all files", "*.*"))
+    data_file = filedialog.askopenfilename(
+        title="Select file to Graph", filetypes=(("csv files", "*.csv"), ("all files", "*.*"))
     )
 
     # pick the file for the settings
-    configFile = filedialog.askopenfilename(
-        initialdir="", title="Select the Graphing Config file", filetypes=(("csv files", "*.csv"), ("all files", "*.*"))
+    config_file = filedialog.askopenfilename(
+        title="Select the Graphing Config file", filetypes=(("csv files", "*.csv"), ("all files", "*.*"))
     )
 
     root.destroy()  # Close the tkinter window
 
     #
-    # Read csv
-    df = pd.read_csv(fileToUse)
-    df.head()
+    # Read data & config
+    df = pd.read_csv(data_file)  # Read data
 
-    with open(configFile, 'r') as s:  # makes the config file into a list of lists
-        reader = csv.reader(s)
-        configMainList = list(reader)
+    with open(config_file, 'r') as file:
+        data_to_plot, relevant_intervals  = list(csv.reader(file))  # Read config file
 
     #
     # Get ready to plot data
-    configList = configMainList[0]  # pulls the first list of which headers to graph together
-    intervalList = configMainList[1]  # pulls the second list of what time interval to graph
-
-    minLimit = float(intervalList[0])  # gets the min and max limts for graphing
-    maxLimit = float(intervalList[1])
+    minLimit = float(relevant_intervals[0])  # gets the min and max limts for graphing
+    maxLimit = float(relevant_intervals[1])
 
     #
     # Plot data
-    for dataToPlot in configList:  # plots each data point based on the other settings
+    for dataToPlot in data_to_plot:  # plots each data point based on the other settings
         maxTime = df['secFromStart'] < maxLimit
         minTime = df['secFromStart'] > minLimit
+
         new = df[maxTime & minTime]
+
         x = new['secFromStart']
         y = new[dataToPlot]
 
         plt.plot(x, y)
 
-    plt.legend()  # Display legend for plot
+    plt.legend()  # Enable legend
 
     plt.show()  # Show the matplotlib plot
 
