6ff050e1e5be6aa1893f15a54de8a10ec87727d3
csdhv9 <csdhv9@mst.edu>
2018-10-28 14:09:36 -0500

    Add more sample code to scrollframe.

--- tools/config_maker/scroll_frame.py
+++ tools/config_maker/scroll_frame.py
@@ -1,3 +1,65 @@
+root.grid_rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+frame_main = tk.Frame(root, bg="gray")
+frame_main.grid(sticky='news')
+
+label1 = tk.Label(frame_main, text="Label 1", fg="green")
+label1.grid(row=0, column=0, pady=(5, 0), sticky='nw')
+
+label2 = tk.Label(frame_main, text="Label 2", fg="blue")
+label2.grid(row=1, column=0, pady=(5, 0), sticky='nw')
+
+label3 = tk.Label(frame_main, text="Label 3", fg="red")
+label3.grid(row=3, column=0, pady=5, sticky='nw')
+
+# Create a frame for the canvas with non-zero row&column weights
+frame_canvas = tk.Frame(frame_main)
+frame_canvas.grid(row=2, column=0, pady=(5, 0), sticky='nw')
+frame_canvas.grid_rowconfigure(0, weight=1)
+frame_canvas.grid_columnconfigure(0, weight=1)
+# Set grid_propagate to False to allow 5-by-5 buttons resizing later
+frame_canvas.grid_propagate(False)
+
+# Add a canvas in that frame
+canvas = tk.Canvas(frame_canvas, bg="yellow")
+canvas.grid(row=0, column=0, sticky="news")
+
+# Link a scrollbar to the canvas
+vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
+vsb.grid(row=0, column=1, sticky='ns')
+canvas.configure(yscrollcommand=vsb.set)
+
+# Create a frame to contain the buttons
+frame_buttons = tk.Frame(canvas, bg="blue")
+canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
+
+# Add 9-by-5 buttons to the frame
+rows = 9
+columns = 5
+buttons = [[tk.Button() for j in xrange(columns)] for i in xrange(rows)]
+for i in range(0, rows):
+    for j in range(0, columns):
+        buttons[i][j] = tk.Button(frame_buttons, text=("%d,%d" % (i+1, j+1)))
+        buttons[i][j].grid(row=i, column=j, sticky='news')
+
+# Update buttons frames idle tasks to let tkinter calculate buttons sizes
+frame_buttons.update_idletasks()
+
+# Resize the canvas frame to show exactly 5-by-5 buttons and the scrollbar
+first5columns_width = sum([buttons[0][j].winfo_width() for j in range(0, 5)])
+first5rows_height = sum([buttons[i][0].winfo_height() for i in range(0, 5)])
+frame_canvas.config(width=first5columns_width + vsb.winfo_width(),
+                    height=first5rows_height)
+
+# Set the canvas scrolling region
+canvas.config(scrollregion=canvas.bbox("all"))
+
+
+
+
+
+
 from tkinter import *
 
 
@@ -22,22 +84,22 @@ class VerticalScrolledFrame(Frame):
 
         vscrollbar.grid(column=10, rowspan=10)
 
-        canvas = Canvas(self, bd=0, highlightthickness=0,
+        self.canvas = Canvas(self, bd=0, highlightthickness=0,
                         yscrollcommand=vscrollbar.set)
 
         # TODO - Separate canvas and scrollbar?, sticky?
-        #canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
-        #canvas.grid(column=0)
+        #self.canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        #self.canvas.grid(column=0)
 
-        vscrollbar.config(command=canvas.yview)
+        vscrollbar.config(command=self.canvas.yview)
 
         # reset the view
-        canvas.xview_moveto(0)
-        canvas.yview_moveto(0)
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
 
         # create a frame inside the canvas which will be scrolled with it
-        self.interior = interior = Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior,
+        self.interior = interior = Frame(self.canvas)
+        interior_id = self.canvas.create_window(0, 0, window=interior,
                                            anchor=NW)
 
         # track changes to the canvas and frame width and sync them,
@@ -45,14 +107,14 @@ class VerticalScrolledFrame(Frame):
         def _configure_interior(event):
             # update the scrollbars to match the size of the inner frame
             size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
+            self.canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
                 # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
+                self.canvas.config(width=interior.winfo_reqwidth())
         interior.bind('<Configure>', _configure_interior)
 
         def _configure_canvas(event):
-            if interior.winfo_reqwidth() != canvas.winfo_width():
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
                 # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-        canvas.bind('<Configure>', _configure_canvas)
+                self.canvas.itemconfigure(interior_id, width=self.canvas.winfo_width())
+            self.canvas.bind('<Configure>', _configure_canvas)
