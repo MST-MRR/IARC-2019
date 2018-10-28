a9126dc3694fe453afa9c2da14851dd940042a49
csdhv9 <csdhv9@mst.edu>
2018-10-28 15:43:46 -0500

    Added default values to time intervals. Switch when config & data selector show up.

--- tools/csv_grapher/csvFileGrapher.py
+++ tools/csv_grapher/csvFileGrapher.py
@@ -21,19 +21,22 @@ def make_whole_graph():
     # Select data to graph through explorer prompt
     # TODO - From config??
 
+    # Way to start of zoomed
+
     #
     #
     # TODO - Make read in associated files based on config -> if only one config file read it
-
-    data_file = filedialog.askopenfilename(
-        title="Select file to Graph", filetypes=(("csv files", "*.csv"), ("all files", "*.*"))
-    )
 
     # pick the file for the settings
     # TODO - By default?
 
     config_file = filedialog.askopenfilename(
-        title="Select the Graphing Config file", filetypes=(("xml files", "*.xml"), ("csv files", "*.csv"), ("all files", "*.*"))
+        title="Select the Graphing Config file",
+        filetypes=(("xml files", "*.xml"), ("csv files", "*.csv"), ("all files", "*.*"))
+    )
+
+    data_file = filedialog.askopenfilename(
+        title="Select file to Graph", filetypes=(("csv files", "*.csv"), ("all files", "*.*"))
     )
 
     root.destroy()  # Close the tkinter window
@@ -45,15 +48,14 @@ def make_whole_graph():
     # Read data & config
     df = pd.read_csv(data_file)  # Read data
 
-    config = parse_config(config_file)
+    config = parse_config(config_file)[0]
 
-    data_to_plot = [metric['x_stream'] for metric in config[0]['metric']]
-    relevant_intervals = [config[0]['lower_time'], config[0]['upper_time']]
+    data_to_plot = [metric['x_stream'] for metric in config['metric']]
 
     #
-    # Get ready to plot data
-    min_time_limit = float(relevant_intervals[0])  # gets the min and max limts for graphing
-    max_time_limit = float(relevant_intervals[1])
+    # Prepare data
+    min_time_limit = float(config['lower_time']) if config['lower_time'] else 0
+    max_time_limit = float(config['upper_time']) if config['upper_time'] else 100000
 
     rows_below_max = df['secFromStart'] < max_time_limit
     rows_above_min = df['secFromStart'] > min_time_limit
