07e8cade272919237181e52e46e6df9673ee3b64
csdhv9 <csdhv9@mst.edu>
2018-12-05 16:00:26 -0600

    Convert to built in logging.

--- tools/ipc/interprocess_communication.py
+++ tools/ipc/interprocess_communication.py
@@ -2,76 +2,113 @@ import subprocess
 import threading
 from time import sleep
 
-# TODO - Make sure everything works from multiple start locations
-
-# TODO - ERROR, WARNING, INFO
-# TODO - Colorama or logging
+import logging
 
 # TODO - How to tell whether to use rtg or logger
+# TODO - How to tell what logging level to use
 
-# TODO - Able to use with statement?
+# TODO - Make sure everything works from multiple start locations
+# TODO - Pip install module or does requirements work?
 
 
 class IPC:
     """
     Takes data sent to it and sends it to the datasplitter running in python 3.6
     Meant to run in python 2.7
-    Python 3 must be able to be run from python3 command
+    Python 3 must be able to be run from py3command's command
 
+    Parameters
+    ----------
+    reader: bool, default=True
+        Whether or not to use the shell reader
+
+    thread_stop: threading.Event, default=threading.Event
+        The thread stop to be used by shell reader, can pass in own thread stop or allow it to create its own.
     """
 
-    def __init__(self, thread_stop=threading.Event()):
+    py3command = 'python3'  # Command to start specifically python3 (rename python.exe in Python3 folder to python3.exe)
+
+    def __init__(self, reader=True, thread_stop=threading.Event()):
+        self.thread_stop = thread_stop
+
+        self.reader_thread = threading.Thread(target=self.shell_reader)
+
         for filename in ['tools/ipc/data_splitter.py', 'data_splitter.py']:
-            self.splitter = subprocess.Popen('python3 {}'.format(filename), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            if reader:
+                self.splitter = subprocess.Popen('{} {}'.format(IPC.py3command, filename), stdin=subprocess.PIPE,
+                                                 stdout=subprocess.PIPE)
+            else:
+                self.splitter = subprocess.Popen('{} {}'.format(IPC.py3command, filename), stdin=subprocess.PIPE)
 
             sleep(.1)  # Poll will return None(None means process working) if immediately called after creating obj
             if not self.splitter.poll():
                 break  # Success!
             else:
                 output, error_output = self.splitter.communicate()
-                print(error_output)
+                logging.warning(error_output)
         else:
             # If loops through file names & doesnt find data splitter
+            logging.error("Data splitter file not found!")
             raise IOError("Data splitter file not found!")
-            return
 
-        self.thread_stop = thread_stop
+        if reader:
+            self.start_reader()
 
-        self.threads = {'output_reader': threading.Thread(target=self.shell_reader)}
+    def __enter__(self):
+        """
+        On enter with statement
+        """
 
-        for thread in self.threads.values():
-            thread.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        On exit with statement
+        """
+
+        self.quit()
 
     def quit(self):
         """
         Terminates subprocess and created thread
         """
 
-        print("IPC: Done sending data.")
+        logging.info("IPC: Quitting.")
         demo.splitter.terminate()
 
         self.thread_stop.set()
 
-        for thread in self.threads.values():
-            thread.join()
+        try:
+            self.reader_thread.join()
+        except RuntimeError:
+            pass
 
     def send(self, data):
         """
+        Send data to splitter
 
         Parameters
         ----------
         data:
             Data in format rtg and or logger can read
         """
+
         if self.splitter.poll() is None:
             try:
                 self.splitter.stdin.write("{}\n".format(str(data).encode()))  # Lots of warnings against stdin!
             except IOError as e:
-                print(e)
+                logging.warning(e)
 
-            print("IPC: {}".format(str(data)))
+            logging.info("IPC: {}".format(str(data)))
         else:
-            print("IPC: Cannot send data")
+            logging.error("IPC: Cannot send data")
+
+    def start_reader(self):
+        """
+        Starts the thread for shell_reader to execute
+        """
+
+        self.reader_thread.start()
 
     def shell_reader(self):
         """
@@ -80,38 +117,36 @@ class IPC:
 
         while not self.thread_stop.is_set():
             out = self.splitter.stdout.readline().strip()  # output w/out \n
-            if not out == "": print(out)
+            if not out == "":
+                print(out)  # Just printing out, let other process choose what to output
 
 
 if __name__ == '__main__':
-
     from math import sin, cos
 
-    demo = IPC()
+    logging.basicConfig(level=logging.INFO)
 
-    for j in range(10000):
-        i = j / 3.14
-        demo.send({
-            'airspeed': cos(i),
-            'velocity_x': sin(i),
-            'velocity_y': cos(i),
-            'velocity_z': sin(i),
-            'altitude': sin(i),
-            'target_altitude': sin(i),
-            'roll': cos(i),
-            'pitch': sin(i),
-            'yaw': cos(i),
-            'target_roll_velocity': cos(i),
-            'target_pitch_velocity': cos(i),
-            'target_yaw': sin(i),
-            'voltage': cos(i)
-        })
+    with IPC() as demo:
+        for i in range(0, 10000, 3):
+            # i = j / 3.14
+            demo.send({
+                'airspeed': cos(i),
+                'velocity_x': sin(i),
+                'velocity_y': cos(i),
+                'velocity_z': sin(i),
+                'altitude': sin(i),
+                'target_altitude': sin(i),
+                'roll': cos(i),
+                'pitch': sin(i),
+                'yaw': cos(i),
+                'target_roll_velocity': cos(i),
+                'target_pitch_velocity': cos(i),
+                'target_yaw': sin(i),
+                'voltage': cos(i)
+            })
 
-        sleep(.1)
+            sleep(.1)
 
-        if demo.splitter.poll() is not None or demo.thread_stop.is_set():
-            print("IPC: splitter.poll(): {}".format(demo.splitter.poll()))
-            break
-
-    demo.quit()
-
+            if demo.splitter.poll() is not None or demo.thread_stop.is_set():
+                logging.info("IPC: splitter.poll(): {}".format(demo.splitter.poll()))
+                break
