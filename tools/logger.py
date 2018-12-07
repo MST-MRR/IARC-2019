eae7ccb933d186f0d81269284bfcc042fc836811
csdhv9 <csdhv9@mst.edu>
2018-12-06 21:37:51 -0600

    Update how logger works.

--- tools/logger.py
+++ tools/logger.py
@@ -13,18 +13,16 @@ class Logger:
 
     Parameters
     ----------
-    desired_data: dict
-        Pair of headers and data streams. Time is set as first header by default.
+    desired_data: list
+        List of data streams as keys it should look for in data received. Time is set as first header by default.
     """
     def __init__(self, desired_data=None):
 
         #
         # Setup dict w/ headers matched to desired data stream
-        self.desired_data = {'secFromStart': None}
-        self.desired_data.update(
-            {'airspeed': 'airspeed',  'altitude': 'altitude', 'pitch': 'pitch', 'roll': 'roll', 'yaw': 'yaw',
-             'velocity_x': 'velocity_x', 'velocity_y': 'velocity_y', 'velocity_z': 'velocity_z', 'voltage': 'voltage'}
-            if not desired_data else desired_data)
+        self.desired_data = ['secFromStart']
+        self.desired_data += ['airspeed', 'altitude', 'pitch', 'roll', 'yaw', 'velocity_x', 'velocity_y',
+                              'velocity_z', 'voltage'] if not desired_data else desired_data
 
         #
         # Setup directory name
@@ -47,7 +45,7 @@ class Logger:
         # Create file
         self.logging_file = open(self.directory, "w")
 
-        self.writer = csv.DictWriter(self.logging_file, fieldnames=self.desired_data.keys())
+        self.writer = csv.DictWriter(self.logging_file, fieldnames=self.desired_data)
 
         self.writer.writeheader()
 
@@ -82,7 +80,7 @@ class Logger:
         current_time = time.time()
 
         if self.last_update_time != current_time:
-            self.writer.writerow({key: input_data[element] if element else current_time - self.start_time for key, element in self.desired_data.items()})
+            self.writer.writerow({element: input_data[element] if element is not 'secFromStart' else current_time - self.start_time for element in self.desired_data})
 
         self.last_update_time = current_time
 
@@ -96,9 +94,7 @@ if __name__ == '__main__':
 
     stopWhile = 0
 
-    my_logger = Logger({'airspeed': 'airspeed',  'altitude': 'altitude', 'pitch': 'pitch', 'roll': 'roll', 'yaw': 'yaw',
-             'velocity_x': 'velocity_x', 'velocity_y': 'velocity_y', 'velocity_z': 'velocity_z',
-            'voltage': 'voltage'})
+    my_logger = Logger()
 
     func = lambda x: math.cos(x)
 
