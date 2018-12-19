13859a222cb7a27a9f6b9047497e4fdff0ad462f
csdhv9 <csdhv9@mst.edu>
2018-12-18 20:51:35 -0600

    Merge from base_tools.

--- tools/file_oi/file_io.py
+++ tools/file_oi/file_io.py
@@ -1,4 +1,12 @@
-from xml.etree.ElementTree import parse as parse_xml
+import xml.etree.ElementTree as ET
+from xml.dom.minidom import parseString
+
+
+def xml_to_string(xml_tag):
+    """
+    Makes xml objects into string formatted in a readable way
+    """
+    return parseString(ET.tostring(xml_tag)).toprettyxml(indent="  ")
 
 
 def parse_config(filename):
@@ -19,7 +27,7 @@ def parse_config(filename):
 
     output = []
 
-    root = parse_xml(filename).getroot()
+    root = ET.parse(filename).getroot()
 
     for graph in root.findall('graph'):
         output.append({
@@ -47,3 +55,66 @@ def parse_config(filename):
             })
 
     return output
+
+
+def possible_metrics(filename):
+    """
+    Returns list of possible metrics and their data.
+
+    Parameters
+    ----------
+    filename: str
+        Filename to read possible metrics from.
+
+    Returns
+    -------
+    List of possible metrics. List of dictionaries -> {label: [(x_stream, y_stream, z_stream), func]}
+    """
+
+    raw_data = parse_config(filename)
+
+    metrics = {
+        metric['label']: [(metric['x_stream'], metric['y_stream'], metric['z_stream']), metric['func']]
+        for graph in raw_data for metric in graph['metric']}
+
+    return metrics
+
+
+def write_config(filename, data):
+    """
+    Writes config file in xml format.
+
+    Parameters
+    ----------
+    filename: str
+        Filename to be saved as.
+
+    data: list of dictionaries
+        List of graphs and all of their data.
+    """
+
+    # Encode data
+    desiredgraphs = ET.Element('desiredgraphs')
+
+    for graph in data:
+        curr_graph = ET.SubElement(desiredgraphs, 'graph', {key: value for key, value in graph.items() if type(value) is not list and value})
+        for key, lst in [(key, value) for key, value in graph.items() if type(value) is list and value]:
+            for item in lst:
+                ET.SubElement(curr_graph, key, {key: value for key, value in item.items() if value})
+
+    # Write
+    with open(filename, 'w') as g:
+        g.write(xml_to_string(desiredgraphs))
+
+
+if __name__ == '__main__':
+    def test_parse_config():
+        print(parse_config('test_config1.xml'))
+
+    def test_write_config():
+        write_config('test_config2.xml', parse_config('test_config1.xml'))
+
+    def test_possible_metrics():
+        print(possible_metrics('test_config1.xml'))
+
+    test_possible_metrics()
