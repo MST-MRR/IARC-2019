f0603d30f2766ab07f324ba89878d09d3b7558bb
csdhv9 <csdhv9@mst.edu>
2018-12-19 14:35:43 -0600

    Add rtg cache.

---
+++ tools/rtg_cache.py
@@ -0,0 +1,19 @@
+
+
+
+class RTGCache:
+    """
+
+    """
+
+    def __init__(self):
+        # self.rtg = rtg
+        # self.rtg.set_pull_function(self.pull)
+        pass
+
+    def pull(self):
+        """
+        Pull stored data
+        """
+
+        return self.data
