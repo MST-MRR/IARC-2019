67fe0af72a793c5053cd3dde4be041f7223855ea
csdhv9 <csdhv9@mst.edu>
2019-01-04 16:25:43 -0600

    Add comment.

--- tools/data_splitter.py
+++ tools/data_splitter.py
@@ -22,6 +22,8 @@ class DataSplitter:
 
     def __init__(self, logger_desired_headers=[], use_rtg=True):
 
+        # Enable or disable each tool based on parameter choice
+
         if logger_desired_headers is [] or not logger_desired_headers:
             logging.critical("Splitter: Logger disabled.")
             self.logger = None
