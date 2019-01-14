ed4d7e638b5ad3121f2463ad60825f62ed04c848
csdhv9 <csdhv9@mst.edu>
2019-01-14 17:58:22 -0600

    Update unit test.

--- tools/unit_tests/test_ipc.py
+++ tools/unit_tests/test_ipc.py
@@ -13,10 +13,10 @@ from interprocess_communication import IPC
 
 
 if __name__ == '__main__':
-    print("IPC: Should graph randomly for 10 seconds.")
+    print("IPC: Should graph randomly for a few seconds.")
 
     with IPC() as demo:
-        for i in range(0, 100, 3):
+        for i in range(0, 500, 3):
             demo.send(get_demo_data())
 
             sleep(.1)
