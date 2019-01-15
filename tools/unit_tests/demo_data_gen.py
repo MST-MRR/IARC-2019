4271bbf17f0498e5e82cfd6ebe361f7b8aacf9ef
csdhv9 <csdhv9@mst.edu>
2019-01-14 19:51:35 -0600

    Made iterable.

--- tools/unit_tests/demo_data_gen.py
+++ tools/unit_tests/demo_data_gen.py
@@ -13,33 +13,14 @@ def get_demo_data():
 
     gen = random.rand(26, 1)
 
-    imaginary_data = {
-        'altitude': gen[0, 0],
-        'airspeed': gen[1, 0],
-        'velocity_x': gen[2, 0],
-        'velocity_y': gen[3, 0],
-        'velocity_z': gen[4, 0],
-        'voltage': gen[5, 0],
-        'state': gen[6, 0],
-        'mode': gen[7, 0],
-        'armed': gen[8, 0],
-        'roll': gen[9, 0],
-        'pitch': gen[10, 0],
-        'yaw': gen[11, 0],
-        'altitude_controller_output': gen[12, 0],
-        'altitude_rc_output': gen[13, 0],
-        'target_altitude': gen[14, 0],
-        'pitch_controller_output': gen[15, 0],
-        'pitch_rc_output': gen[16, 0],
-        'target_pitch_velocity': gen[17, 0],
-        'roll_controller_output': gen[18, 0],
-        'roll_rc_output': gen[19, 0],
-        'target_roll_velocity': gen[20, 0],
-        'yaw_controller_output': gen[21, 0],
-        'yaw_rc_output': gen[22, 0],
-        'target_yaw': gen[23, 0],
-        'color_image': gen[24, 0],
-        'depth_image': gen[25, 0]
-    }
+    keys = [
+        'altitude', 'airspeed', 'velocity_x', 'velocity_y', 'velocity_z', 'voltage', 'state', 'mode', 'armed', 'roll',
+        'pitch', 'yaw', 'altitude_controller_output', 'altitude_rc_output', 'target_altitude',
+        'pitch_controller_output', 'pitch_rc_output', 'target_pitch_velocity', 'roll_controller_output',
+        'roll_rc_output', 'target_roll_velocity', 'yaw_controller_output', 'yaw_rc_output', 'target_yaw', 'color_image',
+        'depth_image'
+    ]
+    
+    imaginary_data = {key: gen[i, 0] for i, key in enumerate(keys)}
 
     return imaginary_data
