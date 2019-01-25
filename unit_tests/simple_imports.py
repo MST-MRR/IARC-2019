4f982e1541c3d7c60c3726f6ccf4797b6fc394a7
csdhv9 <csdhv9@mst.edu>
2019-01-24 22:46:57 -0600

    Added functions for individual tasks.

--- unit_tests/simple_imports.py
+++ unit_tests/simple_imports.py
@@ -2,4 +2,13 @@ import os, sys, inspect
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
-sys.path.insert(0, parent_dir)
+def import_parent():
+    sys.path.insert(0, parent_dir)
+
+
+def import_distributor():
+    directory = parent_dir + r"\tools\data_distributor\app"
+
+    print(directory)
+
+    sys.path.insert(0, directory)
