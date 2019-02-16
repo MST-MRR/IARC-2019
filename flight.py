078d0cad9dc0f58dd4f65aa7c0e00a5c4a1bbf15
Clay McGinnis <mcginnis@claymav.com>
2019-02-15 20:12:15 -0600

    Flight now safer and using TCP

--- flight.py
+++ flight.py
@@ -1,6 +1,7 @@
 """CLI wrapper for flight, runs flight code using HTTP commands"""
 import argparse
 import importlib
+import json
 import logging
 import socket
 import sys
@@ -8,7 +9,32 @@ import threading
 
 import flight.constants as c
 import flightconfig as fc
-from flight.drone.drone_controller import DroneController
+
+# from flight.drone.drone_controller import DroneController
+
+
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
+def set_keepalive_osx(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    sends a keepalive ping once every 3 seconds (interval_sec)
+    """
+    # scraped from /usr/include, not exported by python's socket module
+    TCP_KEEPALIVE = 0x10
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
 
 
 class InvalidDirectionException(Exception):
@@ -75,6 +101,7 @@ def debug_add_task(controller, command, meta):
 
     if command == "exit":
         controller.add_exit_task(c.Priorities.HIGH)
+        return False
     elif command == "land":
         controller.add_land_task(priority)
     elif command == "hover":
@@ -83,6 +110,7 @@ def debug_add_task(controller, command, meta):
         controller.add_takeoff_task(meta["altitude"])
     elif command == "move":
         controller.add_linear_movement_task(direction, meta["time"], priority)
+    return True
 
 
 def get_priority(priority):
@@ -109,7 +137,7 @@ def start_ai(routine):
     importlib.import_module("flight.AIs.{}".format(routine))
 
 
-def kill_ai():
+def kill_ai(controller):
     pass
 
 
@@ -121,18 +149,18 @@ def add_task(args, controller, data):
     if args.debug:
         # If in debug mode
         if not ("command" in data and "meta" in data):
-            return "command and meta required"
+            logging.error("command and meta required")
+            return True
         command = data["command"].lower()
         meta = data["meta"]
         try:
-            debug_add_task(controller, command, meta)
-            return "success"
+            return debug_add_task(controller, command, meta)
         except (InvalidDirectionException, InvalidPriorityException) as err:
             return err
         except Exception as err:
             logging.error("Unexpected error, killing drone")
             debug_add_task(controller, "exit", {})
-            return "Unexpected error, killing drone " + str(err)
+            return False
     elif args.routine:
         # If in production mode, starting or stopping drone is only option
         # get push data and check if start or kill
@@ -142,44 +170,53 @@ def add_task(args, controller, data):
             start_ai(args.routine)
         elif command == "kill":
             # force into land
-            kill_ai()
+            kill_ai(controller)
 
         return "routine " + args.routine + " in progress"
 
 
 def tcp_thread(args, controller):
-    TCP_IP = '127.0.0.1'
-    TCP_PORT = 5005
-    BUFFER_SIZE = 1024
+    try:
+        TCP_IP = '127.0.0.1'
+        TCP_PORT = 5005
+        BUFFER_SIZE = 1024
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        set_keepalive_osx(s)
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(1)
 
-    conn, addr = s.accept()
-    print('Connection address:', addr)
-    while True:
-        data = conn.recv(BUFFER_SIZE)
-        print("received data:", data)
-        conn.send(data)  # echo
-    conn.close()
-    # kill DroneController
+        conn, addr = s.accept()
+        print('Connection address:', addr)
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+            print("received data:", data)
+            if data:
+                succ = add_task(args, controller, json.loads(data))
+                if succ:
+                    conn.send("success")
+                else:
+                    logging.info("Ending session")
+                    conn.close()
+    except socket.error:
+        # kill DroneController
+        print("Killing drone because of connection loss")
 
 
 def main():
     # Get Command Line Arguments
     args = parse_args()
 
-    controller = None
     # Establish controller
     # controller = DroneController(c.Drones.LEONARDO_SIM)
     # Run Flask server on seperate thread
-    server_thread = threading.Thread(
-        target=tcp_thread, args=(args, controller))
-    server_thread.daemon = True
+    # server_thread = threading.Thread(
+    #     target=tcp_thread, args=(args, controller))
+    # server_thread.daemon = True
 
-    server_thread.start()
+    # server_thread.start()
     # controller.run()
+    tcp_thread(args, None)
 
     # After controller has stopped, exit Flask server.
     sys.exit(0)
--- tools/flight_cli/client.py
+++ tools/flight_cli/client.py
@@ -1,40 +1,59 @@
+import json
 import logging
+import socket
 import sys
 
 import requests
 
-if len(sys.argv) < 2:
-    logging.error("Please specify a host")
-    sys.exit(1)
-
-URL = 'http://{}/commands/'.format(sys.argv[1])
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
 
 
-def gen_req(command, **kwargs):
-    resp = requests.post(URL, json={"command": command, "meta": kwargs})
-    if resp.status_code != requests.codes.ok:
-        logging.error(resp.text)
+def gen_req(s, command, **kwargs):
+    message = json.dumps({"command": command, "meta": kwargs})
+    s.send(message.encode())
 
 
-while True:
-    PARAMS = input("> ").split()
-    try:
-        COMMAND = PARAMS[0]
-        if COMMAND == "exit":
-            gen_req(COMMAND)
-        elif COMMAND == "land":
-            gen_req(COMMAND, priority=PARAMS[1])
-        elif COMMAND == "hover":
-            gen_req(COMMAND, priority=PARAMS[2], time=PARAMS[1])
-        elif COMMAND == "takeoff":
-            gen_req(COMMAND, altitude=PARAMS[1])
-        elif COMMAND == "move":
-            gen_req(
-                COMMAND,
-                priority=PARAMS[3],
-                direction=PARAMS[1],
-                time=PARAMS[2])
-        else:
-            logging.warning("Not a command. Try again.")
-    except Exception as err:
-        logging.error(err)
+def main():
+    if len(sys.argv) < 2:
+        logging.error("Please specify a host")
+        sys.exit(1)
+
+    TCP_HOST = sys.argv[1]
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_HOST, TCP_PORT))
+
+    while True:
+        PARAMS = input("> ").split()
+        try:
+            COMMAND = PARAMS[0]
+            if COMMAND == "exit":
+                gen_req(s, COMMAND)
+                s.shutdown(socket.SHUT_WR)
+                s.close()
+                return
+            elif COMMAND == "land":
+                gen_req(s, COMMAND, priority=PARAMS[1])
+            elif COMMAND == "hover":
+                gen_req(s, COMMAND, priority=PARAMS[2], time=PARAMS[1])
+            elif COMMAND == "takeoff":
+                gen_req(s, COMMAND, altitude=PARAMS[1])
+            elif COMMAND == "move":
+                gen_req(
+                    s,
+                    COMMAND,
+                    priority=PARAMS[3],
+                    direction=PARAMS[1],
+                    time=PARAMS[2])
+            else:
+                logging.warning("Not a command. Try again.")
+            data = s.recv(BUFFER_SIZE)
+            if not data:
+                raise Exception
+        except Exception as err:
+            logging.error(err)
+
+
+if __name__ == "__main__":
+    main()
