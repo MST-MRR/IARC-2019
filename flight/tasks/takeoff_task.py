e83faeb6f0160b7c597ad0350dec8b98d1823ea3
Thomas McKanna <thomasmckanna@gmail.com>
2019-02-21 15:27:53 -0600

    TakeoffTask state based instead of using bools.

--- flight/tasks/takeoff_task.py
+++ flight/tasks/takeoff_task.py
@@ -1,6 +1,12 @@
+from enum import Enum
+import time
+
 from task_base import TaskBase
 from .. import constants as c
 from ... import flightconfig as f
+
+ALTITUDE_EPSILON = 0.1 # Acceptable error between measured altitude and target altitude
+POST_TAKEOFF_HOVER_DURATION = 2 # How long to hover after takeoff in seconds
 
 class TakeoffTask(TaskBase):
     """A task that takes off the drone from the ground. This task is intended
@@ -10,16 +16,16 @@ class TakeoffTask(TaskBase):
     ----------
     _target_alt : float
         How many meters off the ground to take off to.
-    _count : int
-        Keeps track of how many more iterations this task should be called on
-        before it returns true (i.e. finished).
-    _began_takeoff : bool
-        Set to true after the drone has been armed but not yet finished takeoff.
+    _state_index : int
+        The current index into the takeoff tasks agenda. See next attribute.
+    _agenda : list of functions
+        The ordering in which to execute the various parts of take off.
 
     Notes
     -----
     This task will not work on the simulated drone.
     """
+
     def __init__(self, drone, altitude):
         """Initialize a task for taking off.
 
@@ -32,19 +38,75 @@ class TakeoffTask(TaskBase):
         """
         super(TakeoffTask, self).__init__(drone)
         self._target_alt = altitude
-        self._count = 10 / c.DELAY_INTERVAL
-        self._began_takeoff = False
+        self._state_index = 0
+        self._agenda = [self._pre_takeoff_procedure,
+                        self._send_takeoff_procedure,
+                        self._takeoff_procedure,
+                        self._post_takeoff_procedure
+                        ]
 
     def perform(self):
+        """Do one iteration of logic for taking off the drone."""
+        # Call the function associated with current state
+        result = self._agenda[self._state_index]()
+        # If returned true, move on to the next state
+        if result is True:
+            self._state_index += 1
+        # Return whether or not the task is done (all states finished)
+        return self._state_index == len(self._agenda)
+
+    def _pre_takeoff_procedure(self):
+        """Actions taken before take off begins.
+
+        Checks that the drone is armed.
+
+        Returns
+        -------
+        True if the drone has been armed, and False otherwise.
+        """
         if not self._drone.armed:
             self._drone.arm()
-        elif not self._began_takeoff:
-            self._drone.simple_takeoff(self._target_alt)
-            self._began_takeoff = True
+            return False
         else:
-            self._count -= 1
+            return True
 
-        if self._count <= 0:
+    def _send_takeoff_procedure(self):
+        """Sends the takeoff MAVLink message to the drone.
+
+        Returns
+        -------
+        True upon success, and False otherwise.
+        """
+        self._drone.simple_takeoff(self._target_alt)
+        return True
+
+    def _takeoff_procedure(self):
+        """Actions as the drone is moving towards the target altitude.
+
+        Checks whether or not the drone has reached its target altitude.
+
+        Returns
+        -------
+        True if the drone has reached its target altitude, and False otherwise.
+        """
+        if abs(self._drone.rangefinder.distance - self._target_alt) < ALTITUDE_EPSILON:
+            self._start_hover_time = time.time()
+            self._drone.send_velocity(0, 0, 0) # Hover
             return True
         else:
             return False
+
+    def _post_takeoff_procedure(self):
+        """Actions after the drone has ascending to its target altitude.
+
+        Checks whether or not the post takeoff hover duration has elapsed.
+
+        Returns
+        -------
+        True if the drone has hovered long enough, and False otherwise.
+        """
+        if abs(time.time() - self._start_hover_time) < POST_TAKEOFF_HOVER_DURATION:
+            self._drone.send_velocity(0, 0, 0) # Resend hover message
+            return False
+        else:
+            return True
