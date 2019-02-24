f7e855c4320ddec7c6013537860645b31c5d7c84
colesteroni <csdhv9@mst.edu>
2019-02-24 14:29:39 -0600

    Cleanup code.

--- accum/vbo.cpp
+++ accum/vbo.cpp
@@ -9,8 +9,9 @@ using namespace glm;
 
 #include "opennn.h"
 
+
 int main(){
-	// Initialise GLFW
+	// Init GLFW
 	glewExperimental = true; // Needed for core profile
 	if( !glfwInit() )
 	{
@@ -21,14 +22,14 @@ int main(){
 	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
 
-	// Open a window and create its OpenGL context
-	GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
-	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+	// Open window & add OpenGL context
+	GLFWwindow* window;
+	window = glfwCreateWindow( 1024, 768, "Accumulator", NULL, NULL);
 	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
 		glfwTerminate();
 		return -1;
 	}
@@ -40,71 +41,62 @@ int main(){
 		return -1;
 	}
 
-	// Ensure we can capture the escape key being pressed below
+	// Ensure can capture escape key press
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
 
-// Create and compile our GLSL program from the shaders
-GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
 
+	// 3f/vertex
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		-1.0f, 0.0f, 0.0f,
+		1.0f,  0.0f, 0.0f,
+	};
+	GLuint v_count = 6;
 
-GLuint VertexArrayID;
-glGenVertexArrays(1, &VertexArrayID);
-glBindVertexArray(VertexArrayID);
-// An array of 3 vectors which represents 3 vertices
-/* Reference
-static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
-   -1.0f, 1.0f, 0.0f,
-   0.0f, -1.0f, 0.0f,
-   1.0f,  1.0f, 0.0f,
-};*/
-static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, 1.0f, 0.0f,
-   -1.0f,  1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   -1.0f, 0.0f, 0.0f,
-   1.0f,  0.0f, 0.0f,
-};
-
-GLuint v_count = 6;
-
-// This will identify our vertex buffer
-GLuint vertexbuffer;
-// Generate 1 buffer, put the resulting identifier in vertexbuffer
-glGenBuffers(1, &vertexbuffer);
-// The following commands will talk about our 'vertexbuffer' buffer
-glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-// Give our vertices to OpenGL.
-glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
+	// This will identify our vertex buffer
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 
 	do{
-		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		// Clear screen
 		glClear( GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
 		glClearColor(0.0f, 0.0f, 0.4f, 0.0f) ;
-		// Draw nothing, see you in tutorial 2 !
+		
+		// Set buffer
 		// 1st attribute buffer : vertices
-glEnableVertexAttribArray(0);
-glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-glVertexAttribPointer(
-   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-   3,                  // size
-   GL_FLOAT,           // type
-   GL_FALSE,           // normalized?
-   0,                  // stride
-   (void*)0            // array buffer offset
-);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 
-// Use our shader
-glUseProgram(programID);
-// Draw the triangle !
-glDrawArrays(GL_LINES, 0, v_count); // Starting from vertex 0; 3 vertices total -> 1 triangle
-glDisableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,          // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,          // size
+			GL_FLOAT,   // type
+			GL_FALSE,   // normalized?
+			0,          // stride
+			(void*)0	// array buffer offset
+		);
+
+		// Use our shader
+		glUseProgram(programID);
+			
+		// Draw
+		glDrawArrays(GL_LINES, 0, v_count); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -112,7 +104,7 @@ glDisableVertexAttribArray(0);
 
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		glfwWindowShouldClose(window) == 0 );
+		   glfwWindowShouldClose(window) == 0 );
 
 	return 0;
 }
