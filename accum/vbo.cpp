7dccc85b0a70f2852825e24cda8c1355bdfa5295
colesteroni <csdhv9@mst.edu>
2019-02-26 18:47:18 -0600

    Made variables private.

--- accum/vbo.cpp
+++ accum/vbo.cpp
@@ -14,7 +14,7 @@ using namespace glm;
 
 class SickOpenGL{
 // make into a class and make sure state set before tryna do stuff
-  public:
+  private:
  	GLFWwindow* window;
   	const GLfloat g_vertex_buffer_data[18] = {
 		-1.0f, -1.0f, 0.0f,
@@ -26,98 +26,98 @@ class SickOpenGL{
 	};
 	GLuint v_count = 6;
 
+  public:
+	SickOpenGL(){
+		// Init GLFW
+		glewExperimental = true; // Needed for core profile
+		if( !glfwInit() )
+		{
+			fprintf( stderr, "Failed to initialize GLFW\n" );
+			throw std::runtime_error("Failed to initialize GLFW.");
+		}
 
-  SickOpenGL(){
-	// Init GLFW
-	glewExperimental = true; // Needed for core profile
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		throw std::runtime_error("Failed to initialize GLFW.");
-	}
+		glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
 
-	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
-
-	// Open window & add OpenGL context
-	
-	window = glfwCreateWindow( 1024, 768, "Accumulator", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
-		glfwTerminate();
-		throw std::runtime_error("Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.");
-	}
-	glfwMakeContextCurrent(window); // Initialize GLEW
-	
-	glewExperimental=true; // Needed in core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		throw std::runtime_error("Failed to initialize GLEW");
-	}
-
-	// Ensure can capture escape key press
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-  }
-  ~SickOpenGL(){
-
-  }
-  void run(){
-	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-	
-	// This will identify our vertex buffer
-	GLuint vertexbuffer;
-	// Generate 1 buffer, put the resulting identifier in vertexbuffer
-	glGenBuffers(1, &vertexbuffer);
-	// The following commands will talk about our 'vertexbuffer' buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	// Give our vertices to OpenGL.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-
-	do{
-		// Clear screen
-		glClear( GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.0f, 0.0f, 0.4f, 0.0f) ;
+		// Open window & add OpenGL context
 		
-		// Set buffer
-		// 1st attribute buffer : vertices
-		glEnableVertexAttribArray(0);
+		window = glfwCreateWindow( 1024, 768, "Accumulator", NULL, NULL);
+		if( window == NULL ){
+			fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
+			glfwTerminate();
+			throw std::runtime_error("Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.");
+		}
+		glfwMakeContextCurrent(window); // Initialize GLEW
+		
+		glewExperimental=true; // Needed in core profile
+		if (glewInit() != GLEW_OK) {
+			fprintf(stderr, "Failed to initialize GLEW\n");
+			throw std::runtime_error("Failed to initialize GLEW");
+		}
+
+		// Ensure can capture escape key press
+		glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	}
+	~SickOpenGL(){
+
+	}
+	void run(){
+		// Create and compile our GLSL program from the shaders
+		GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+
+		GLuint VertexArrayID;
+		glGenVertexArrays(1, &VertexArrayID);
+		glBindVertexArray(VertexArrayID);
+		
+		// This will identify our vertex buffer
+		GLuint vertexbuffer;
+		// Generate 1 buffer, put the resulting identifier in vertexbuffer
+		glGenBuffers(1, &vertexbuffer);
+		// The following commands will talk about our 'vertexbuffer' buffer
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		// Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(
-			0,          // attribute 0. No particular reason for 0, but must match the layout in the shader.
-			3,          // size
-			GL_FLOAT,   // type
-			GL_FALSE,   // normalized?
-			0,          // stride
-			(void*)0	// array buffer offset
-		);
 
-		// Use our shader
-		glUseProgram(programID);
+		do{
+			// Clear screen
+			glClear( GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
+			glClearColor(0.0f, 0.0f, 0.4f, 0.0f) ;
 			
-		// Draw
-		glDrawArrays(GL_LINES, 0, v_count); // Starting from vertex 0; 3 vertices total -> 1 triangle
-		glDisableVertexAttribArray(0);
+			// Set buffer
+			// 1st attribute buffer : vertices
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+			glVertexAttribPointer(
+				0,          // attribute 0. No particular reason for 0, but must match the layout in the shader.
+				3,          // size
+				GL_FLOAT,   // type
+				GL_FALSE,   // normalized?
+				0,          // stride
+				(void*)0	// array buffer offset
+			);
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+			// Use our shader
+			glUseProgram(programID);
+				
+			// Draw
+			glDrawArrays(GL_LINES, 0, v_count); // Starting from vertex 0; 3 vertices total -> 1 triangle
+			glDisableVertexAttribArray(0);
 
-	//return 0;
-  }
+			// Swap buffers
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+		} // Check if the ESC key was pressed or the window was closed
+		while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+			glfwWindowShouldClose(window) == 0 );
+
+		//return 0;
+	}
 };
 
 
