b155a87754fb0ed7c55af67a23f7e8179b190b8b
colesteroni <csdhv9@mst.edu>
2019-03-14 23:46:53 -0500

    Add notes on why test failed and commented test code.

--- accum/test3/atomic-counter.glsl
+++ accum/test3/atomic-counter.glsl
@@ -1,10 +1,23 @@
 #version 420 core
 
-layout (r32ui) uniform uimageBuffer counter_buffer;  // not layout in book
+// layout (binding=0, offset=0) uniform atomic_uint countboi;
+// uint counter =  atomicCounterIncrement(image_buffer);
+/*
+// should be 2 of the same things separated by 40px but wont shift
+// https://stackoverflow.com/questions/14285849/trouble-with-imagestore-opengl-4-3
 
-//layout (binding=0, offset=0) uniform atomic_uint countboi;
-// offset += 4 for next atomic
+// test p574
+layout (binding = 0, r32ui) uniform uimageBuffer colors;
+layout (binding = 1, rgba32f) uniform image2D output_buffer;
+out vec4 color;
 
+  vec4 col = imageLoad(colors, gl_PrimitiveID &255);
+  imageStore(output_buffer, ivec2(gl_FragCoord.xy) - ivec2(200,0), col);
+  imageStore(output_buffer, ivec2(gl_FragCoord.xy) + ivec2(200,0), col);
+  color = vec4(1.0,0.0, 0.0,1);
+*/
+
+layout (r32ui) uniform uimageBuffer image_buffer;  
 
 uniform sampler2D my_texture;
 
@@ -12,19 +25,27 @@ in vec2 tex_coord;
 
 layout (location=0) out vec4 fragment_color;
 
+// not double counting intersections & middle vertical lines
+
+
+// Now trying shader buffer storage p576
+
+// do i try overdraw counting with shader buffer or what
+
 void main(void){
+
     vec4 texel_color = texture(my_texture, tex_coord);
+
+    // May change from atomic adding to imageLoad or something meant for texture
+    // pg 572
     
-    uint counter = imageAtomicAdd(counter_buffer, 0, 1);
-
-    //uint counter =  atomicCounterIncrement(countboi);
-    // atomic counter returns c's prior value!
-
-    if(counter > 20000000){
+    uint counter = imageAtomicAdd(image_buffer, 0, 1);
+    
+    if(counter > 300000000){  // Since not resetting each frame
       fragment_color = vec4(
-        1.0,//tex_coord.x, //0.0, // - texel_color.r,
-        0.0,  //1.0,// - texel_color.g,
-        0.0,  //1.0,// - texel_color.b,
+        1.0,
+        0.0, 
+        0.0,
         1);
     } else {
       fragment_color = vec4(
@@ -34,5 +55,3 @@ void main(void){
         1);
     }
 }
-
-// check book pg 574
