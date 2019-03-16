1e2a2ac1271e3b6348430c25a881c00656e2289f
colesteroni <csdhv9@mst.edu>
2019-03-16 00:30:53 -0500

    Add code from other repo in test 4. Make changes to test 3.

--- accum/test4/run.sh
+++ accum/test4/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-g++ -std=c++17 -Wall main.cpp loader.cpp -L/usr/local/lib -lglfw -pthread -lGLEW -lGLU -lGL -lrt -lXrandr -lXxf86vm -lXi -lXinerama -lX11 -o OpenGLExample
+g++ -std=c++17 -Wall main.cpp loader.cpp -ggdb -g -lglfw -lGL -lGLEW -lfreeimage -o OpenGLExample
 ./OpenGLExample
