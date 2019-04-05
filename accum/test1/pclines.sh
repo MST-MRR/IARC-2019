a254b56d4699e4131403ef09be3fcc08358f86b0
colesteroni <csdhv9@mst.edu>
2019-04-04 22:17:50 -0500

    Remove all previous tests.

--- accum/test1/pclines.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clear
-g++ -std=c++17 -Wall vbo.cpp use_shaders.cpp -ggdb -g -lglfw -lGL -lGLEW -lfreeimage -o OpenGLExample
-./OpenGLExample
--- accum/test2/main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clear
-g++ -std=c++17 -Wall main.cpp  -lGL -lGLU -lglut -o OpenGLExample
-./OpenGLExample
