7091d71ee4d78be9c0fe323cec3132d678b83a44
colesteroni <colesteronid@gmail.com>
2019-05-28 01:02:46 -0500

    Add conversion rule.

--- vision/ts_converter.py
+++ vision/ts_converter.py
@@ -13,13 +13,12 @@ def binarize_mat(img):
 
 	Returns
 	-------
-	Mat with all values in [0, 1]
+	Mat with all values in [0, 1].
 	"""
 
 	img = np.where(img > .5, 1., 0.)
 
 	return img
-
 
 
 def get_ts_verticies(edges):
@@ -35,27 +34,17 @@ def get_ts_verticies(edges):
 	-------
 	List of pairs of 3 tuples representing verticies of lines.
 	"""
+	u = 5
+	rule = lambda x, y: ((-u, -y), (0, x), (u, y))
+
+	print(rule(1, 1))
 
 	# for each edge in mat, create 2 lines
 
 
 
+
 if __name__ == '__main__':
-	x, y = np.meshgrid(np.linspace(-1,1,512), np.linspace(-1,1,512))
-	d = np.sqrt(x*x+y*y)
-	sigma, mu = 1.0, 0.0
-	gradient = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
+	edges = cv2.imread('edges.jpg')
 
-	img = binarize_mat(gradient)
-
-	cv2.imshow('image', img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-	"""
-	edges = cv2.imread('edges.png')
-
-	edges = binarize_mat(edges)
-
-	get_ts_verticies(edges)
-	"""
+	get_ts_verticies(binarize_mat(edges))
