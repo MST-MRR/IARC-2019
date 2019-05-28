5b497b0b81f5d046424d9eebe5b4ed92ed1ae8be
colesteroni <colesteronid@gmail.com>
2019-05-27 23:30:05 -0500

    Add ts_converter.py.

---
+++ vision/ts_converter.py
@@ -0,0 +1,26 @@
+
+
+def get_ts_verticies(edges):
+	"""
+	Convert edge coordinates to verticies of lines in TS space.
+
+	Parameters
+	----------
+	edges: mat
+		OpenCV mat with edges = 1.
+
+	Returns
+	-------
+	List of pairs of 3 tuples representing verticies of lines.
+	"""
+
+	# get all edges of mat
+
+	# convert edges to verticies
+
+
+
+if __name__ == '__main__':
+	edges = imread('edges.png')
+
+	get_ts_verticies(edges)
