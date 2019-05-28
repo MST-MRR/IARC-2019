31d30d5cf47847bbf0729b0757c1cd3e4c5e8b5b
colesteroni <colesteronid@gmail.com>
2019-05-27 23:03:48 -0500

    Change input and add output. Add main.

--- vision/edges.py
+++ vision/edges.py
@@ -2,13 +2,17 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-def get_edges(image_path):
-    image = cv2.imread(image_path)
+def get_edges(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((5,5),np.float32)/5
     dst = cv2.filter2D(gray,-1,kernel)
     lap = cv2.Laplacian(dst,cv2.CV_64F)
-    cv2.imwrite('gray.jpg', lap)
+    
+    return lap
 
 
-
+if __name__ == '__main__':
+	qr = cv2.imread('code.png', cv2.IMREAD_UNCHANGED)
+	cv2.imshow('code', get_edges(qr))
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
