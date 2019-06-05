9dbf3141bc8f33db5af38f8e32f5883873c4a801
colesteroni <colesteronid@gmail.com>
2019-06-05 03:54:24 -0500

    Fix indentation.

--- vision/unit_tests/test_qr_gen.py
+++ vision/unit_tests/test_qr_gen.py
@@ -6,7 +6,6 @@ python -m pip install zbar
 """
 
 
-
 import unittest
 
 import os, sys
@@ -17,28 +16,28 @@ import qrtools
 
 
 class TestQRGen(unittest.TestCase):
-	"""
-	QR code generator tester.
-	"""
+    """
+    QR code generator tester.
+    """
 
-	def test_generator(self):
-		"""
-		Test the qr generator by reading multiple generated codes.
-		"""
-		for code in ['1003', '1234']:
-			qr_code = QrCode(code)
+    def test_generator(self):
+        """
+        Test the qr generator by reading multiple generated codes.
+        """
+        for code in ['1003', '1234']:
+            qr_code = QrCode(code)
 
-			path = qr_code.save(qr_code.img)
-			print(path)
-			interpreter = qrtools.QR()
-			interpreter.decode(path)
+            path = qr_code.save(qr_code.img)
+            print(path)
+            interpreter = qrtools.QR()
+            interpreter.decode(path)
 
-			interpreted_code = interpreter.data
+            interpreted_code = interpreter.data
 
-			self.assertEqual(code, interpreted_code)
+            self.assertEqual(code, interpreted_code)
 
-			os.remove(path)
+            os.remove(path)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
