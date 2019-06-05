af103a3b0f843058b2a2068fdb8d4eb3d0cb37f3
colesteroni <colesteronid@gmail.com>
2019-06-05 03:52:58 -0500

    Fix indentation.

--- vision/qr_gen/QrCodeUsage.py
+++ vision/qr_gen/QrCodeUsage.py
@@ -16,24 +16,24 @@ from argparse import ArgumentParser
 
 
 if __name__ == '__main__':
-	parser = ArgumentParser()
+    parser = ArgumentParser()
 
-	parser.add_argument("-q", type=str, dest="code",
-	                    help="String to encode.")
+    parser.add_argument("-q", type=str, dest="code",
+                        help="String to encode.")
 
-	options = parser.parse_args()
+    options = parser.parse_args()
 
-	default_code = '1003'
+    default_code = '1003'
 
-	# It's not necessary for this to be a string here, it should work with any value
-	# that has a valid strign representation.
-	qr_code = QrCode(options.code if options.code else default_code)
-	# Shows the QR code
-	qr_code.show()
-	# Saves the QR code with its 4 segments separated out to an image in the current
-	# directory
-	qr_code.save()
-	# Waits indefinitely until a key is pressed.
-	cv2.waitKey(0)
-	# Clean up any windows left over by OpenCV tasks.
-	cv2.destroyAllWindows()
+    # It's not necessary for this to be a string here, it should work with any value
+    # that has a valid strign representation.
+    qr_code = QrCode(options.code if options.code else default_code)
+    # Shows the QR code
+    qr_code.show()
+    # Saves the QR code with its 4 segments separated out to an image in the current
+    # directory
+    qr_code.save()
+    # Waits indefinitely until a key is pressed.
+    cv2.waitKey(0)
+    # Clean up any windows left over by OpenCV tasks.
+    cv2.destroyAllWindows()
