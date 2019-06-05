67e8be66adb8cc006df4e5e12c2d0817e829a833
colesteroni <colesteronid@gmail.com>
2019-06-05 03:59:10 -0500

    Fix indentation.

--- vision/ts_converter.py
+++ vision/ts_converter.py
@@ -3,52 +3,52 @@ import numpy as np
 
 
 def binarize_mat(img):
-	"""
-	Convert mat to binary based on threshold.
+    """
+    Convert mat to binary based on threshold.
 
-	Parameters
-	----------
-	img: mat
-		OpenCV mat.
+    Parameters
+    ----------
+    img: mat
+    	OpenCV mat.
 
-	Returns
-	-------
-	Mat with all values in [0, 1].
-	"""
+    Returns
+    -------
+    Mat with all values in [0, 1].
+    """
 
-	img = np.where(img > .5, 1., 0.)
+    img = np.where(img > .5, 1., 0.)
 
-	return img
+    return img
 
 
 def get_ts_verticies(edges, u=10., z=1.):
-	"""
-	Convert edge coordinates to verticies of lines in TS space.
+    """
+    Convert edge coordinates to verticies of lines in TS space.
 
-	Parameters
-	----------
-	edges: mat
-		1 channel binary OpenCV mat.
-	u: float
-		Scale of u axis.
-	z: float
-		Z value.
+    Parameters
+    ----------
+    edges: mat
+    	1 channel binary OpenCV mat.
+    u: float
+    	Scale of u axis.
+    z: float
+    	Z value.
 
-	Returns
-	-------
-	List of pairs of 3 tuples representing verticies of lines.
-	"""
-	rule = lambda x, y: [(-u, -y, z), (0., x, z), (0., x, z), (u, y, z)]
+    Returns
+    -------
+    List of pairs of 3 tuples representing verticies of lines.
+    """
+    rule = lambda x, y: [(-u, -y, z), (0., x, z), (0., x, z), (u, y, z)]
 
-	verticies = []
-	for index, value in np.ndenumerate(edges):
-		if value == 1:
-			verticies += rule(*index)
+    verticies = []
+    for index, value in np.ndenumerate(edges):
+    	if value == 1:
+    		verticies += rule(*index)
 
-	return verticies
+    return verticies
 
 
 if __name__ == '__main__':
-	edges = cv2.imread('edges.jpg', 0)
+    edges = cv2.imread('edges.jpg', 0)
 
-	get_ts_verticies(binarize_mat(edges))
+    get_ts_verticies(binarize_mat(edges))
