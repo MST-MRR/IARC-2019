e43d4d40dd165b415ada1ea6497bdc2705e34a7f
colesteroni <colesteronid@gmail.com>
2019-06-17 01:23:28 -0500

    Implement full(broken) pipeline .

--- vision/qr-pipeline.py
+++ vision/qr-pipeline.py
@@ -6,26 +6,33 @@ import numpy as np
 from generator.QrCode import QrCode
 from normalize.edges import get_edges
 from normalize.ts_converter import get_ts_verticies, binarize_mat
+from accumulator.py_to_cpp import TS
 
 
 if __name__ == '__main__':
-	# 3. Compartmentalize qr code normalizer.
-	# 4. Normalize qr code in this file.
-	# 5. Put cpp into accumulator/source, put other accum stuff into accumulator/
-	# 6. Run accumulator on normalized code.
-	value = '1234'
+    value = '1234'
 
-	generator = QrCode(value)
-	
-	# switch to each corners
-	image = generator.img
+    generator = QrCode(value)
 
-	# cv2.imshow("qr", image)
-	# cv2.waitKey(0)
-	
-	edges = binarize_mat(get_edges(image), threshold=.5)
+    # switch to each corners
+    image = generator.img
 
-	# cv2.imshow("edges", edges)
-	# cv2.waitKey(0)
-	
-	verticies = get_ts_verticies(edges, u=10, z=0.) #if z needs to be float ensure it in ts_converter.
+    # cv2.imshow("qr", image)
+    # cv2.waitKey(0)
+
+    edges = binarize_mat(get_edges(image), threshold=.5)
+
+    # cv2.imshow("edges", edges)
+    # cv2.waitKey(0)
+
+    verticies = get_ts_verticies(edges, u=10, z=0.)
+
+    VCOUNT = len(verticies) // 3
+ 
+    space = TS(1024, 768, VCOUNT, verticies.ctypes.data)
+    img = space.accumulate()
+
+    img = np.where(img > 0, .2, 0.)
+
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
