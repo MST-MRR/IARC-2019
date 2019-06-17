e9a77b4a9f5d7fb35c9acd7cdcb2457aa0d8b25c
colesteroni <colesteronid@gmail.com>
2019-06-17 01:12:18 -0500

    Move accumulator source to accumulator/source.

--- vision/accumulator/run.sh
+++ vision/accumulator/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-g++ -std=c++17 -Wall accumulator/main.cpp accumulator/shader_loader.cpp -ggdb -g -lglfw -lGL -lGLEW -lopencv_core -lopencv_highgui -lopencv_imgproc -o QRDetector.exe
+g++ -std=c++17 -Wall accumulator/source/main.cpp accumulator/source/shader_loader.cpp -ggdb -g -lglfw -lGL -lGLEW -lopencv_core -lopencv_highgui -lopencv_imgproc -o QRDetector.exe
 ./QRDetector.exe
